d925569cdcf73b991daffefff05e8ebf3ab8efc6
FreqFox <freqfox@tailspace.app>
2026-03-25 22:37:57 +0100

    Update layout.tsx
    Change brandname

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -15,10 +15,10 @@ import "../modules/ui/globals.css";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Formbricks",
-    default: "Formbricks",
+    template: "%s | TailSpace",
+    default: "TailSpace",
   },
-  description: "Open-Source Survey Suite",
+  description: "Forms",
 };
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
